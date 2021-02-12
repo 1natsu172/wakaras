3ad83c16dc768f975003980e4eaa52fcc83c5755
1natsu172 <1natsu172@users.noreply.github.com>
2021-02-13 00:35:19 +0900

    promptだして勝利

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,14 +1,15 @@
-import { browser, Runtime } from "webextension-polyfill-ts"
+import { browser, Runtime } from 'webextension-polyfill-ts'
 import waitElement from '@1natsu/wait-element'
 import moji from 'moji'
 
-console.log('wakaras script start');
+console.log('wakaras script start')
 
-const delay = (ms: number) => new Promise<void>(resolve => {
-  setTimeout(() => {
-    resolve()
-  }, ms);
-})
+const delay = (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
 
 function isUserDetailInfo(path: string) {
   return path.includes('UserDetailInfo')
@@ -16,33 +17,53 @@ function isUserDetailInfo(path: string) {
 
 window.addEventListener('load', async () => {
   if (isUserDetailInfo(window.location.pathname)) {
-    const kairiButton = document.querySelector<HTMLElement>('#kibanaOpen.btn-warning')
+    const kairiButton = document.querySelector<HTMLElement>(
+      '#kibanaOpen.btn-warning',
+    )
 
     if (!kairiButton) {
       return
     }
 
-    kairiButton.click();
+    const theDayTableRowElement = kairiButton.closest('tr')
+    if (!theDayTableRowElement) {
+      throw new Error('rowの親DOM見つからへん')
+    }
 
+    const theDayKairiData = getKairiDataFromRow(theDayTableRowElement)
+
+    kairiButton.click()
 
     // モーダルの出現を待つ
-    const modalOpended = await waitElement('#modalDivergence.modal.fade.in').then(() => delay(300))
+    const modalOpended = await waitElement(
+      '#modalDivergence.modal.fade.in',
+    ).then(() => delay(300))
 
-    const kairiReasonInput = document.querySelector<HTMLInputElement>('#reasontext')
-    const kairiReasonSaveButton = document.querySelector<HTMLButtonElement>('#divergenceSave');
+    const kairiReasonInput = document.querySelector<HTMLInputElement>(
+      '#reasontext',
+    )
+    const kairiReasonSaveButton = document.querySelector<HTMLButtonElement>(
+      '#divergenceSave',
+    )
 
     const reasonSelectDescription = (() => {
-      const selects = Object.entries(reasonOptionMap).map((([key, value]) => {
-        return `${key}: ${value}`
-      }))
-      selects.unshift(`当てはまるものの数字を選んで入力してください\n`)
-      selects.push(`\nその他の理由の場合は、理由をここに書いてください`)
+      const templateSelects = Object.entries(reasonOptionMap).map(
+        ([key, value]) => {
+          return `${key}: ${value}`
+        },
+      )
+
+      const selects = [
+        createKairiDataDescription(theDayKairiData),
+        ...templateSelects,
+      ]
+
       return selects.join('\n')
     })()
 
     const kairiReasonSelected = prompt(reasonSelectDescription)
 
-    console.log(kairiReasonSelected);
+    console.log(kairiReasonSelected)
 
     if (kairiReasonSelected && kairiReasonInput) {
       const kairiReason = convertToKairiReason(kairiReasonSelected)
@@ -55,17 +76,67 @@ window.addEventListener('load', async () => {
   }
 })
 
-
 ////////////////////////////////////////////////////////////////
 /**
  * 乖離データパースしたりなんやら
  */
 ////////////////////////////////////////////////////////////////
 
-function a(rowNode: HTMLElement) {
-
+type KairiData = {
+  date: string
+  kaishi: {
+    dakoku?: string
+    jissai?: string
+  }
+  taikin: {
+    dakoku?: string
+    jissai?: string
+  }
+  kairiTime?: string
 }
 
+function getKairiDataFromRow(rowNode: HTMLTableRowElement): KairiData {
+  const cols = [...rowNode.children] as HTMLElement[]
+  if (!cols.length) {
+    throw new Error(
+      'なんかしらんけどカラムらしきDOM見当たらへんで。行のDOMミスってるか見当違いのDOM渡してへん？',
+    )
+  }
+  const [
+    date,
+    _name,
+    kaishiDakoku,
+    kaishiJissai,
+    taikinDakoku,
+    taikinJissai,
+    _kintaiMinyuryoku,
+    kairiTime,
+  ] = cols.map((col) => col.innerText)
+  return {
+    date,
+    kaishi: {
+      dakoku: kaishiDakoku,
+      jissai: kaishiJissai,
+    },
+    taikin: {
+      dakoku: taikinDakoku,
+      jissai: taikinJissai,
+    },
+    kairiTime,
+  }
+}
+
+function createKairiDataDescription(kairiData: KairiData) {
+  const { date, kaishi, taikin, kairiTime } = kairiData
+  return `テンプレ数字or理由を入力してください
+${date}
+開始の打刻は ${kaishi.dakoku || '?'} でしたが実際は ${kaishi.jissai || '?'
+    } でした。
+退勤の打刻は ${taikin.dakoku || '?'} でしたが実際は ${taikin.jissai || '?'
+    } でした。
+トータルの乖離時間は ${kairiTime} です。
+`
+}
 
 ////////////////////////////////////////////////////////////////
 /**
@@ -74,19 +145,22 @@ function a(rowNode: HTMLElement) {
 ////////////////////////////////////////////////////////////////
 
 const reasonOptionMap = {
-  "1": '退勤後にキーボード清掃のため再度PCを開いたため',
-  "2": '退勤後にオンライン雑談をしていたため',
-  "3": '退勤後に技術の勉強をしていたため',
-  "4": '退勤後にOSS活動をしていたため',
+  '1': '退勤後にキーボード清掃のため再度PCを開いたため',
+  '2': '退勤後にオンライン雑談をしていたため',
+  '3': '退勤後に技術の勉強をしていたため',
+  '4': '退勤後にOSS活動をしていたため',
 } as const
 
 type ReasonOptionKeys = keyof typeof reasonOptionMap
 
 function convertToKairiReason(keyOrReason: string) {
   // 全角文字 to 半角文字
-  const HEkeyOrReason = moji(keyOrReason).convert('ZE', 'HE').toString();
+  const HEkeyOrReason = moji(keyOrReason).convert('ZE', 'HE').toString()
 
-  const isDefinedKey = (HEkeyOrReason: string): HEkeyOrReason is ReasonOptionKeys => Object.keys(reasonOptionMap).includes(HEkeyOrReason)
+  const isDefinedKey = (
+    HEkeyOrReason: string,
+  ): HEkeyOrReason is ReasonOptionKeys =>
+    Object.keys(reasonOptionMap).includes(HEkeyOrReason)
 
   if (isDefinedKey(HEkeyOrReason)) {
     return reasonOptionMap[HEkeyOrReason]
